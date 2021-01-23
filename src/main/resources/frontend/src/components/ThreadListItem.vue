7754f4d1582a5a5733254a85753fc4aaa5905615
André <andreingvarsson@hotmail.com>
2021-01-23 22:59:32 +0100

    lockthread PUT to backend and frontend icon

--- src/main/resources/frontend/src/components/ThreadListItem.vue
+++ src/main/resources/frontend/src/components/ThreadListItem.vue
@@ -2,9 +2,30 @@
   <div class="thread ">
     <div class="card mt-3 container">
       <div @click="navigate" class="card-body row">
-        <h1 class="card-title title-text col-6">{{ thread.title }}</h1>
+        <h1 class="card-title title-text col-5">{{ thread.title }}</h1>
         <p class="card-text message-text col-7">{{ thread.threadMessage }}</p>
         <!-- <span class="col-6 size">{{ thread.timestamp }}</span> -->
+
+        <div class="card-footer">
+          <svg
+            @click.stop="lockThread"
+            v-if="user ? user.roles.includes('ADMIN') : false"
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            class="bi bi-lock "
+            viewBox="0 0 16 16"
+            :class="{
+              isLocked: thread.lockedThread,
+              isOpen: !thread.lockedThread,
+            }"
+          >
+            <path
+              d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   </div>
@@ -20,9 +41,29 @@ class ThreadListItem extends Vue {
   })
   thread;
 
+  get user() {
+    return this.$store.state.loggedInUser;
+  }
+
   navigate() {
     this.$store.commit("setThread", this.thread);
     this.$router.push({ path: `/forum/thread/${this.thread.thread_id}` });
+  }
+
+  async lockThread() {
+    console.log("Inne i lockThread");
+    let response = await fetch(
+      `/api/v1/forums/${this.thread.thread_id}/threads`,
+      {
+        method: "PUT",
+        credentials: "include",
+      }
+    );
+    console.log(response);
+    console.log(this.thread.forum_id);
+    if (response.status === 200) {
+      this.$store.dispatch("fetchForumById", this.thread.forum_id);
+    }
   }
 }
 
@@ -34,7 +75,6 @@ export default ThreadListItem;
   border-style: solid;
   border-width: 1px;
   border-color: rgba(0, 0, 0, 0.61);
-  cursor: pointer;
 }
 
 .title-text {
@@ -53,5 +93,14 @@ export default ThreadListItem;
   font-size: 13px;
   display: flex;
   justify-content: flex-start;
+}
+
+.isLocked {
+  color: red;
+}
+
+.isOpen {
+  color: green;
+  cursor: pointer;
 }
 </style>
