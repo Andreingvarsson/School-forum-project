b1d9941fb62c2a1eb12e0b3994d545cc8c945a1d
André <andreingvarsson@hotmail.com>
2021-01-26 15:29:11 +0100

    @Notempty for username and email, requirements for password

--- src/main/java/com/example/demo/dtos/UserDto.java
+++ src/main/java/com/example/demo/dtos/UserDto.java
@@ -4,12 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
+    @NotEmpty
     private String username;
+    @NotEmpty
     private String email;
+    @NotEmpty @Size(min = 6, max = 12)
     private String password;
     private String roles;
 }
--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+
 import javax.persistence.*;
 import java.util.List;
 import java.util.Set;
